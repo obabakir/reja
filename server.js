4a2c469b19bcaa9fede5f12b5f84c70a45e7489f
obabakir <bubufighting01@gmail.com>
2026-04-19 22:45:53 +0900

    BRR: build express server

---
+++ server.js
@@ -0,0 +1,28 @@
+console.log("Hello, World! This is the server.js file.");
+const express = require("express");
+const app = express();
+const http = require("http");
+// 1 KIRISH CODES
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// 2 SESSION CODES
+// 3 VIEWS CODES
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// 4 ROUTING CODES
+app.get("/", function (req, res) {
+  res.end("<h1>Hello world with Abubakir</h1>");
+});
+app.get("/hi", function (req, res) {
+  res.end(`<h1 style='color: blue;'>Hello you are on hi page now</h1>`);
+});
+
+const server = http.createServer(app);
+let PORT = 3003;
+server.listen(PORT, function () {
+  console.log(`Server is running successfullyon port ${PORT}`);
+});
