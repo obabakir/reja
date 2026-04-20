8395ce033f6063845a046cde0cd84a9f034cd5bb
obabakir <bubufighting01@gmail.com>
2026-04-20 14:30:29 +0900

    feat: develop router ejspage

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log("Hello, World! This is the server.js file.");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
 // 1 KIRISH CODES
 
 app.use(express.static("public"));
@@ -14,14 +25,20 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 
 // 4 ROUTING CODES
+
 // app.get("/", function (req, res) {
 //   res.end("<h1>Hello world with Abubakir</h1>");
 // });
-app.post("/create-item", function (req, res) {
-  console.log(req.body);
-  res.json({ test: "success" });
+
+// app.post("/create-item", function (req, res) {
+//   console.log(req.body);
+//   res.json({ test: "success" });
+// });
+app.post("/create-item", (req, res) => {});
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.render("harid");
 });
 
